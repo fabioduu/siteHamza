4e4c11920420054950e82f18179e2fc18c59af4f
fabioduu <fabiodljunior@gmail.com>
2024-02-12 20:22:25 -0300

    asf

--- send_email.php
+++ send_email.php
@@ -1,23 +1,22 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $message = trim($_POST["message"]);
 
-$errors = [];
+    if ($name == "" || $email == "" || $message == "") {
+        echo "Por favor, preencha todos os campos.";
+    } else {
+        $to = "fabiodljunior@gmail.com";
+        $subject = "Nova mensagem de $name";
+        $body = "Nome: $name\nEmail: $email\n\nMensagem:\n$message";
+        $headers = "From: $email";
 
-if (!empty($_POST)) {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
- 
-  if (empty($name)) {
-      $errors[] = 'Name is empty';
-  }
-
-  if (empty($email)) {
-      $errors[] = 'Email is empty';
-  } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $errors[] = 'Email is invalid';
-  }
-
-  if (empty($message)) {
-      $errors[] = 'Message is empty';
-  }
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Mensagem enviada com sucesso!";
+        } else {
+            echo "Erro ao enviar a mensagem. Por favor, tente novamente.";
+        }
+    }
 }
+?>
