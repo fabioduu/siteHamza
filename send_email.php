9919568dadf3dcb9e84f3dc1c6ffbb9579bde62a
fabioduu <fabiodljunior@gmail.com>
2024-02-14 20:37:04 -0300

    fdsf

--- send_email.php
+++ send_email.php
@@ -1,46 +1,32 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-//Load Composer's autoloader
-require 'vendor/autoload.php';
-//get data from form
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-// preparing mail content
-$messagecontent ="Name = ". $name . "<br>Email = " . $email . "<br>Message =" . $message;
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-try {
-    //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'username_here';                     //SMTP username
-    $mail->Password   = 'password_here';                               //SMTP password
-   // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-   // $mail->addAttachment('photo.jpeg', 'photo.jpeg');    //Optional name
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = $messagecontent;
-    
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-   // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
+
+//varible setting
+ $name= $_REQUEST['name'];
+ $email = $_REQUEST['Email'];
+ $message = $_REQUEST['Message'];
+ $subject = "Message from Contact form !";
+
+ $to ="fabiodljunior@gmail.com";  // change receiving email id 
+ 
+ $content = "Name : ". $name. "\r\nContact email : ". $email. "\r\n \r\nMessage : \r\n \r\n".$message ; // name [break] email [break] message
+ 
+
+
+// check input fields
+if ( empty($name)|| empty($email)|| empty($message))
+{
+echo"<script type='text/javascript'>alert('Please fill all correct');
+    window.history.go(-1);
+    </script>";
 }
+else 
+{ mail($to,$subject,$content);
+
+    echo"<script type='text/javascript'>alert('Your message sent succesfully ');
+    window.history.go(-1);
+    </script>";
+}
+
+
+?>
